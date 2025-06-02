fe9f7e113352164b8caa3199ab5733c16ec21503
sikap <ksiya1306@gmail.com>
2025-06-02 23:41:19 +0530

    Initial commit - Calculator App

---
+++ app/src/main/java/com/example/calculator/CalculatorScreen.kt
@@ -0,0 +1,18 @@
+package com.example.calculator
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview(showBackground = true)
+@Composable
+fun CalculatorScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+
+    }
+}
